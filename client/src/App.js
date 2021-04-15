f0d905c2d40bcdf349dd637cfdb15365af0243f6
brad curtis <bradcurtis74@gmail.com>
2021-04-15 08:46:15 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import {listLogEntries} from './api'
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+    width: 1200,
+    height: 1200,
     latitude: 28.921221000,
     longitude: -82.046362000,
     zoom: 15
@@ -33,7 +33,8 @@ const App = () => {
           <Marker longitude={entry.location.coordinates[0]} 
           latitude={entry.location.coordinates[1]}
           offsetLeft={-20}>
-<div>{entry.house}</div>
+            <div>{entry.house}</div>
+<svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
 
  </Marker>
             
